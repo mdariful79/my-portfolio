072e40761526121de84b9d05549f45d2f4dc5c0b
mdariful79 <mdariful057@gmail.com>
2023-10-22 00:10:32 +0600

    Testimonial & contactMe Section

---
+++ client/src/PortfolioContainer/Testimonial/Testimonial.js
@@ -0,0 +1,177 @@
+import React from "react";
+import OwlCarousel from "react-owl-carousel";
+import "owl.carousel/dist/assets/owl.carousel.css";
+import "owl.carousel/dist/assets/owl.theme.default.css";
+
+import ScreenHeading from "../../utilities/ScreenHeading/ScreenHeading";
+import ScrollService from "../../utilities/ScrollService";
+import Animations from "../../utilities/Animations";
+import "./Testimonial.css";
+
+export default function Testimonial(props) {
+  let fadeInScreenHandler = (screen) => {
+    if (screen.fadeScreen !== props.id) return;
+    Animations.animations.fadeInScreen(props.id);
+  };
+  const fadeInSubscription =
+    ScrollService.currentScreenFadeIn.subscribe(fadeInScreenHandler);
+
+  const options = {
+    loop: true,
+    margin: 0,
+    nav: true,
+    animateIn: "bounceInRight",
+    animateOut: "bounceOutRight",
+    dots: true,
+    autoplay: true,
+    smartSpeed: 1000,
+    responsive: {
+      0: {
+        item: 1,
+      },
+      768: {
+        item: 1,
+      },
+      1000: {
+        item: 3,
+      },
+    },
+  };
+
+  return (
+    <div>
+      <ScreenHeading
+        title={"Testimonial"}
+        subHeading={"What my Client Say About me!"}
+      />
+      <section className="testimonial-section" id={props.id || ""}>
+        <div className="container">
+          <div className="row">
+            <OwlCarousel
+              className="owl-carousel"
+              id="testimonial-carousel"
+              {...options}
+            >
+              <div className="col-lg-12">
+                <div className="testi-item">
+                  <div className="testi-comment">
+                    <p>
+                      <i className="fa fa-quote-left" />
+                      I patronized Ariful and when He delivered, I honestly fell
+                      in love with the project He is a very honest guy and he
+                      delivers ontime.
+                      <i className="fa fa-quote-right" />
+                    </p>
+                    <ul className="stars list-unstyled">
+                      <li>
+                        <i className="fa fa-star" />
+                      </li>
+                      <li>
+                        <i className="fa fa-star" />
+                      </li>
+                      <li>
+                        <i className="fa fa-star" />
+                      </li>
+                      <li>
+                        <i className="fa fa-star-half-alt" />
+                      </li>
+                      <li>
+                        <i className="fa fa-star" />
+                      </li>
+                    </ul>
+                  </div>
+                  <div className="client-info">
+                    <img
+                      src="img/Testimonial/lady.png"
+                      alt="No internet connection"
+                    ></img>
+                    <h5>Alina Robert</h5>
+                    <p>CEO Tech Global, U.S.A</p>
+                  </div>
+                </div>
+              </div>
+
+              <div className="col-lg-12">
+                <div className="testi-item">
+                  <div className="testi-comment">
+                    <p>
+                      <i className="fa fa-quote-left" />
+                      I patronized Ariful and when He delivered, I honestly fell
+                      in love with the project He is a very honest guy and he
+                      delivers ontime.
+                      <i className="fa fa-quote-right" />
+                    </p>
+                    <ul className="stars list-unstyled">
+                      <li>
+                        <i className="fa fa-star" />
+                      </li>
+                      <li>
+                        <i className="fa fa-star" />
+                      </li>
+                      <li>
+                        <i className="fa fa-star" />
+                      </li>
+                      <li>
+                        <i className="fa fa-star-half-alt" />
+                      </li>
+                      <li>
+                        <i className="fa fa-star" />
+                      </li>
+                    </ul>
+                  </div>
+                  <div className="client-info">
+                    <img
+                      src="img/Testimonial/mike.png"
+                      alt="No internet connection"
+                    ></img>
+                    <h5>Mike Jacson</h5>
+                    <p>CEO Mike Corporation, U.K</p>
+                  </div>
+                </div>
+              </div>
+
+              <div className="col-lg-12">
+                <div className="testi-item">
+                  <div className="testi-comment">
+                    <p>
+                      <i className="fa fa-quote-left" />
+                      I patronized Ariful and when He delivered, I honestly fell
+                      in love with the project He is a very honest guy and he
+                      delivers ontime.
+                      <i className="fa fa-quote-right" />
+                    </p>
+                    <ul className="stars list-unstyled">
+                      <li>
+                        <i className="fa fa-star" />
+                      </li>
+                      <li>
+                        <i className="fa fa-star" />
+                      </li>
+                      <li>
+                        <i className="fa fa-star" />
+                      </li>
+                      <li>
+                        <i className="fa fa-star-half-alt" />
+                      </li>
+                      <li>
+                        <i className="fa fa-star" />
+                      </li>
+                    </ul>
+                  </div>
+                  <div className="client-info">
+                    <img
+                      src="img/Testimonial/man.png"
+                      alt="No internet connection"
+                    ></img>
+                    <h5>Allison Baker</h5>
+                    <p>CTO Tech Junkies, U.S.A</p>
+                  </div>
+                </div>
+              </div>
+            </OwlCarousel>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
+}
